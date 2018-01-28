ea19c139898a87be6ab133d4ea32ab06ef589a5b
PrimozBelej <pbelej90@gmail.com>
2018-01-28 19:51:35 +0100

    Solved two-fer javascript exercise.

---
+++ javascript/two-fer/two-fer.js
@@ -0,0 +1,11 @@
+var TwoFer = function () {};
+
+TwoFer.prototype.twoFer = function (who) {
+  if (typeof who !== 'string') {
+    who = 'you';
+  }
+
+  return 'One for ' + who + ', one for me.';
+};
+
+module.exports = TwoFer;
