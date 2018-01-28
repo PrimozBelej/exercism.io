70b6fb94826288826085de43e1410b4e5513f031
PrimozBelej <pbelej90@gmail.com>
2018-01-29 00:06:27 +0100

    Solved third javascript problem: leap years.

---
+++ javascript/leap/leap.js
@@ -0,0 +1,30 @@
+//
+// This is only a SKELETON file for the "Leap" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+var Year = function (input) {
+  this.value = input;
+};
+
+Year.prototype.isLeap = function () {
+  if (typeof this.value !== "number") {
+    return false;
+  }
+
+  if (this.value % 400 === 0) {
+    return true;
+  }
+  
+  if (this.value % 100 === 0) {
+    return false;
+  }
+  
+  if (this.value % 4 === 0) {
+    return true;
+  }
+
+  return false;
+};
+
+module.exports = Year;
